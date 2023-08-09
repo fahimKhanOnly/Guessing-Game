dbcb7968252c5251070049e4969ac5ab7457061c
fahimKhanOnly <fahimmontacir767@gmail.com>
2023-08-09 11:10:15 +0600

    Reload Button is added

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
-// Finding Elements
 const form = document.querySelector(".play-ground");
 const usrNum = document.querySelector(".numInput");
 const usrBtn = form.querySelector(".submit-btn");
 const totalResults = document.querySelector(".game-results");
 
-// Create, adding Attributes Elements
 const attemptMsg = document.createElement("p");
 totalResults.appendChild(attemptMsg);
 let attemptMsgInit = `Total Remaining Attempt: `;
 attemptMsg.textContent = `${attemptMsgInit} 10`;
-
-
 const resultMsg = document.createElement("p");
 totalResults.appendChild(resultMsg);
-
-
 const wonLostMsg = document.createElement("div");
 totalResults.appendChild(wonLostMsg);
 wonLostMsg.setAttribute("class", "won-lost");
-
-
 const wonMsg = document.createElement("p");
 wonLostMsg.appendChild(wonMsg);
 wonMsg.setAttribute("class", "won-style");
-
-
 const lostMsg = document.createElement("p");
 wonLostMsg.appendChild(lostMsg);
 lostMsg.setAttribute("class", "lost-style");
+const reloadBtn = document.querySelector(".reload");
 
-// Initialize some value
 let attemptsCount = 0;
 let wonCount = 0;
 let lostCount = 0;
-
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -43,10 +32,13 @@ form.addEventListener("submit", (e) => {
         attemptMsg.textContent = `${attemptMsgInit} ${10 - attemptsCount}`;
         usrNum.disabled = true;
         usrNum.style.backgroundColor = "lightgrey";
+        usrNum.style.cursor = "not-allowed";
         usrBtn.disabled = true;
         usrBtn.style.backgroundColor = "gray";
         usrBtn.style.textDecoration = "line-through";
+        usrBtn.style.cursor = "not-allowed";
         usrNum.setAttribute("placeholder", "Game over");
+        reloadBtn.style.display = "block";
     }else attemptMsg.textContent = `${attemptMsgInit}${10 - attemptsCount}`;
     usrNum.value = "";
 });
@@ -63,3 +55,4 @@ const checkingResult = (value) => {
     }
 };
 const randomNum = (limit) => Math.floor(Math.random() * limit + 1);
+reloadBtn.addEventListener("click", () => window.location.reload());
