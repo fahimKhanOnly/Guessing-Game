b6ca1605bce4e750711fc7698e4b33e8e4fa62ee
fahimKhanOnly <fahimmontacir767@gmail.com>
2023-08-09 12:34:01 +0600

    Grid line added on result's description

--- script.js
+++ script.js
@@ -3,15 +3,11 @@ const usrNum = document.querySelector(".numInput");
 const usrBtn = form.querySelector(".submit-btn");
 const totalResults = document.querySelector(".game-results");
 
-const attemptMsg = document.createElement("p");
-totalResults.appendChild(attemptMsg);
+const attemptMsg = totalResults.querySelector(".attempt-msg");
 let attemptMsgInit = `Total Remaining Attempt: `;
 attemptMsg.textContent = `${attemptMsgInit} 10`;
-const resultMsg = document.createElement("p");
-totalResults.appendChild(resultMsg);
-const wonLostMsg = document.createElement("div");
-totalResults.appendChild(wonLostMsg);
-wonLostMsg.setAttribute("class", "won-lost");
+const resultMsg = totalResults.querySelector(".result-msg");
+const wonLostMsg = totalResults.querySelector(".won-lost");
 const wonMsg = document.createElement("p");
 wonLostMsg.appendChild(wonMsg);
 wonMsg.setAttribute("class", "won-style");
@@ -28,6 +24,7 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     checkingResult(usrNum.value);
     attemptsCount++;
+    totalResults.querySelector(".first-hr").style.display = "block";
     if(10 <= attemptsCount){
         attemptMsg.textContent = `${attemptMsgInit} ${10 - attemptsCount}`;
         usrNum.disabled = true;
